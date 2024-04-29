869a5fcf4a5424a165ede14fc88454ce9b18869f
Sanrit0 <ekaitz77@gmail.com>
2024-04-29 16:39:23 +0200

    first commit

---
+++ functions.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+
+def home(dataframe):
+    st.title('Cargatron')
+    st.image('img\puntos-recarga-madrid.jpg',caption='Coche cargando')
+    with st.expander('¿Quieres saber más?'):
+        st.write('Ante el problema climático al que nos enfrentamos, el coche eléctrico se \
+                plantea como una solución posible. Queremos facilitarte que encuentres tu puesto de carga más cercano')
+    with st.echo():
+        st.dataframe(dataframe)
+
+def datos(dataframe):
+    st.map(dataframe,latitude='latidtud',longitude='longitud')
+    st.bar_chart(dataframe.groupby(['DISTRITO'])[['Nº CARGADORES']].sum())
+    st.bar_chart(dataframe.groupby(['OPERADOR'])[['Nº CARGADORES']].sum())
+
+def menu_filtros(dataframe):
+    filtro_distrito = st.sidebar.selectbox('Seleccione un distrito',tuple(dataframe['DISTRITO'].unique()))
+    distrito_cbox = st.sidebar.checkbox('Filtra por distrito')
+    filtro_operador = st.sidebar.selectbox('Seleccione un operador',tuple(dataframe['OPERADOR'].unique()))
+    operador_cbox = st.sidebar.checkbox('Filtra por operador')
+
+    lista_dist = []
+    lista_op = []
+    if distrito_cbox:
+        lista_dist.append(filtro_distrito)
+    else:
+        lista_dist = dataframe['DISTRITO'].unique()
+    
+    if operador_cbox:
+        lista_op.append(filtro_operador)
+    else:
+        lista_op = dataframe['OPERADOR'].unique()
+
+    filtro_cargadores_start,filtro_cargadores_end = st.sidebar.select_slider('Filtra por número de cargadores',options=range(1,dataframe[['Nº CARGADORES']].max()[0]+1),value=(1,dataframe[['Nº CARGADORES']].max()[0]))
+    df_res = dataframe[(dataframe['DISTRITO'].isin(lista_dist))&(dataframe['OPERADOR'].isin(lista_op))&(dataframe['Nº CARGADORES']>=filtro_cargadores_start)&(dataframe['Nº CARGADORES']<=filtro_cargadores_end)]
+    if df_res.shape[0] == 0:
+        st.warning('No hay datos para los filtros indicados')
+        st.stop()
+    else:
+        st.map(df_res,latitude='latidtud',longitude='longitud')
+        if distrito_cbox == False:
+            st.bar_chart(df_res.groupby(['DISTRITO'])[['Nº CARGADORES']].sum())
+        if operador_cbox == False:
+            st.bar_chart(df_res.groupby(['OPERADOR'])[['Nº CARGADORES']].sum())
---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+from functions import home, datos, menu_filtros
+#datos, cargar_datos, menu_filtros
+#st.beta_expander ahora es expander
+# Este es mi script
+
+st.set_page_config(page_title='Cargatron', layout='wide', page_icon=':battery:')
+df = pd.read_csv('data/red_recarga_acceso_publico_2021.csv', sep = ';')
+
+opcion = st.sidebar.selectbox('Seleccione una opción del menu',('Home','Datos','Filtros'))
+st.sidebar.file_uploader("Carga tus propios datos", type= ['.csv'])
+
+match opcion:
+    case 'Home':
+        home(df)
+    case 'Datos':
+        datos(df)
+    case 'Filtros':
+        menu_filtros(df)
+
+
+st.balloons()
